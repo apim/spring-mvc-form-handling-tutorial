f78abb29ab278651505de06875de3da158bbe1ff
Mainak Bhattacharya <mbhattacharya@gmail.com>
2019-09-04 23:12:55 +1000

    .

--- src/main/java/apim/github/tutorial/RegistrationController.java
+++ src/main/java/apim/github/tutorial/RegistrationController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,17 +21,19 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class RegistrationController {
 
 	@Autowired
-	MessageSource messageSource;
+	private MessageSource messageSource;
 
-	/*
-	 * private RegistrationValidator validator;
-	 * 
-	 * @Autowired public void setValidator(RegistrationValidator validator) {
-	 * this.validator = validator; }
-	 * 
-	 * @InitBinder private void initBinder(WebDataBinder binder) {
-	 * binder.setValidator(validator); }
-	 */
+	private RegistrationValidator validator;
+
+	@Autowired
+	public void setValidator(RegistrationValidator validator) {
+		this.validator = validator;
+	}
+
+	@InitBinder
+	private void initBinder(WebDataBinder binder) {
+		binder.setValidator(validator);
+	}
 
 	private String label(String key) {
 		return messageSource.getMessage(key, null, null);
